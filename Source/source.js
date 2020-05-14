98dd33dc1cc65af640d1c4bdb145aa7edca4ab5d
José GIL <jgil83000@gmail.com>
2020-05-14 11:08:58 +0200

    Avec remuneration

---
+++ Source/source.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function validEnvoi() {
+ if (window.document.querySelector("#i_nom").value === "" &&
+ window.document.querySelector("#i_prenom").value === "" ) {
+ alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
+ }
+ else if (window.document.querySelector("#i_email").value === "") {
+ alert("L'email doit être rempli"); // On affiche un message
+ }
+ else {
+ let question = "Souhaitez-vous réellement utiliser l'adresse suivante : "
+ + window.document.querySelector("#i_email").value;
+ if (confirm(question)) {
+ window.document.querySelector("#form_contact").submit(); // OK envoyer
+ }
+ }
+}
+
+
